7da7720852a24b61507130adda68348d70f946f6
dancampb3ll <78802914+dancampb3ll@users.noreply.github.com>
2024-08-06 21:49:01 +0100

    Removed test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-TILE_MAPPINGS = {
-    0: None,
-    1: "overgroundGrid",
-    2: "overgroundGrass",
-    3: "overgroundWater",
-    4: "overgroundBorder",
-    5: "overgroundSmallDungeonLeft",
-    6: "overgroundSmallDungeonRight"
-}
